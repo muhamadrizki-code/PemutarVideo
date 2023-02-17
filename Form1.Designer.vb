ae97988cb743d92f645fa9450652f8a747351b5e
User <User@WINDOWS-3JS7F1P>
2023-02-17 13:26:38 +0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,319 @@
+﻿Imports System.Diagnostics.Eventing.Reader
+Imports System.IO
+Imports System.Runtime.InteropServices
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(201, 58)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(372, 248)
+        Me.AxWindowsMediaPlayer1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(505, 354)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(68, 30)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Play Video"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Button2.Location = New System.Drawing.Point(505, 317)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(68, 34)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Pilih file"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(201, 317)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(298, 34)
+        Me.TextBox1.TabIndex = 3
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "File"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(505, 389)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(68, 32)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Save"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.HideSelection = False
+        Me.TextBox2.Location = New System.Drawing.Point(594, 58)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox2.Size = New System.Drawing.Size(235, 205)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.LimeGreen
+        Me.Button4.Location = New System.Drawing.Point(594, 274)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(117, 31)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "Tampilkan Histori"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.IndianRed
+        Me.Button5.Location = New System.Drawing.Point(726, 274)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(103, 31)
+        Me.Button5.TabIndex = 7
+        Me.Button5.Text = "Hapus Histori"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(610, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(217, 25)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Daftar Direktori Video"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(263, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(246, 39)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Pemutar Video"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.TextBox3.Location = New System.Drawing.Point(344, 396)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(134, 20)
+        Me.TextBox3.TabIndex = 10
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.TextBox4.Location = New System.Drawing.Point(344, 364)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(134, 20)
+        Me.TextBox4.TabIndex = 11
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(267, 367)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(75, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Created At     :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(267, 396)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(77, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Last Updated :"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(862, 601)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents Button1 As Button
+
+
+    Dim filepath As String
+    Dim file_counter As Integer
+    Sub playvideo()
+        AxWindowsMediaPlayer1.URL = filepath
+        AxWindowsMediaPlayer1.Ctlcontrols.play()
+
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        AxWindowsMediaPlayer1.uiMode = "full"
+        filepath = Path.Combine(Application.StartupPath, TextBox1.Text)
+        If (Not File.Exists(filepath)) Then
+            File.WriteAllText(filepath, TextBox1.Text)
+
+        End If
+        file_counter = 1
+        playvideo()
+
+    End Sub
+
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        OpenFileDialog1.Filter = "video | *.mp4"
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            TextBox1.Text = OpenFileDialog1.FileName
+
+
+        End If
+        TextBox3.Text = File.GetCreationTime(TextBox1.Text)
+        TextBox4.Text = File.GetLastWriteTime(TextBox1.Text)
+
+    End Sub
+
+    Friend WithEvents Button3 As Button
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim file As System.IO.StreamWriter
+        file = My.Computer.FileSystem.OpenTextFileWriter("Historivideo.txt", True)
+        file.WriteLine(TextBox1.Text)
+        file.Close()
+
+        MsgBox("direktori berhasil di simpan")
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button4 As Button
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim readfile As String
+
+        If My.Computer.FileSystem.FileExists("Historivideo.txt") Then
+            readfile = My.Computer.FileSystem.ReadAllText("Historivideo.txt")
+            TextBox2.Text = readfile
+        Else
+            Dim file As System.IO.StreamWriter
+            file = My.Computer.FileSystem.OpenTextFileWriter("Historivideo.txt", True)
+            File.WriteLine("")
+            File.Close()
+
+        End If
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Friend WithEvents Button5 As Button
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        My.Computer.FileSystem.DeleteFile("Historivideo.txt")
+        If My.Computer.FileSystem.FileExists("Historivideo.txt") Then
+            MsgBox("Gagal dihapus")
+        Else
+            MsgBox("Berhasil dihapus")
+
+        End If
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
